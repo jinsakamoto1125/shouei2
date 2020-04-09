697087ba08d7059a532e73fddfab664281d1ee8c
sakamotojin1125 <saka1125jnjn@gmail.com>
2020-04-09 15:25:13 +0900

    First commit.

---
+++ index.php
@@ -0,0 +1,167 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>昭栄電気産業</title>
+     <link rel="stylesheet" href="common/common.css">
+     <meta name="viewport" content="width=device-wide, initial-scale=1, shrink-to-fit=no">
+  </head>
+  <body>
+      <header>
+
+        <div class="header-left">
+          <h1>SHO-<span>EI</span></h1>
+        </div>
+       </header>
+     <div class="top-wrapper">
+      <div class="top-wrapper-2">
+        <h1>皆さまの電気をお守りしています</h1>
+        <h2>私たちと一緒に働きませんか？</h2>
+      </div>
+    </div>
+    <div class="lesson-wrapper">
+        <h1>Company</h1>
+        <div class="lesson-wrapper-2">
+        <div class="lesson">
+           <h2>会社名</h2>
+           <h3>株式会社昭栄電気産業</h3>
+          <div class="clear">
+          </div>
+         <p class="lesson-5">所在地</p> <a href="#" class="lesson-6">東京都葛飾区鎌倉3-57-1</a>
+         <div class="clear">
+         </div>
+          <p><sean>設立</sean>昭和58年9月13日</p>
+          <p><span>資本金</span>1000万</p>
+          <p><span>代表者</span>代表取締役</p>
+          <p><sean>社員</sean>200名</p>
+          <p><span>売上高</span>3億</p>
+        </div>
+        <img src="img/build.jpg" alt="" class="lesson-2"  style="">
+       </div>
+       <div class="clear">
+       </div>
+    </div>
+    <div class="good-wrapper">
+      <h1>BUSINESS</h1>
+      <div class="good-wrapper-2">
+           <ul>
+             <li>
+               <div class="item">
+                 <a href="red">
+                   <img src="img/kuren.jpg" alt=""  style="height:450px;">
+                   <p>保守</p>
+                 </a>
+               </div>
+               <div class="item">
+                 <a href="red">
+                   <img src="img/hozenn.jpg" alt="" style="height:225px;">
+                   <p>保全</p>
+                 </a>
+               </div>
+             </li>
+             <li  class="item">
+              <a href="red">
+               <img src="img/hosyu.jpg" alt=""  class="good" style="height:225px;">
+               <p>電気点検</p>
+             </a>
+           </li>
+        </ul>
+         <div class="clear">
+
+         </div>
+      </div>
+    </div>
+    <div class="message-wrapper">
+        <h1>RECRUIT</h1>
+        <div class="message-wrapper-2">
+        <ul>
+          <li class="item-2">
+            <img src="img/saiyou.jpg" alt="" >
+            <a href="red">
+              <p>中途採用</p>
+            </a>
+          </li>
+          <li class="item-2">
+            <img src="img/women.jpg" alt="">
+            <a href="red">
+              <p>アルバイト・パート</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="clear">
+
+    </div>
+    <div class="contact">
+       <div class="contact-2">
+         <h2>ENTRY</h2>
+         <p>昭栄電気産業に関する募集要項とエントリーフォームはこちら。</p>
+         <div class="contact-3">応募する</div>
+         <div class="clear">
+         </div>
+        </div>
+       </div>
+     <footer>
+    <div class="footer">
+      <div class="footer-2">
+      <div class="footer-3">
+        <p>理念</p>
+      </div>
+      <div class="footer-3">
+        <p><span>専門分野</span></p>
+        <p>造船・海洋</p>
+        <p>建設インフラ</p>
+        <p>エレクトロニクス</p>
+        <p>ヘルスケア・ライフサイエンス</p>
+        <p>食</p>
+        <p>生活</p>
+        <div class="clear">
+
+        </div>
+      </div>
+      <div class="footer-3">
+        <p><span>企業情報</span></p>
+        <p>トップメッセージ</p>
+        <p>戦略、中期経営計画</p>
+        <p>国内・海外ネットワーク</p>
+        <p>会社概要</p>
+        <p>沿革</p>
+        <p>大阪本社・西方ギャラリー</p>
+        <div class="clear">
+
+        </div>
+      </div>
+      <div class="footer-3">
+        <p><span>採用情報</span></p>
+        <p>新卒採用</p>
+        <div class="clear">
+
+        </div>
+      </div>
+      <div class="footer-3">
+        <p><span>お問い合わせ</span></p>
+        <p>ニュースリリース</p>
+        <p>プライバシーポリシー</p>
+        <div class="clear">
+
+        </div>
+      </div>
+      <div class="footer-3">
+        <p><span>language</span></p>
+        <div class="clear">
+
+        </div>
+      </div>
+       </div>
+       <div class="clear">
+
+       </div>
+       <div class="footer-4">
+         <p>© 2019 HARADA CORPORATION All rights Reserved.</p>
+       </div>
+        </div>
+      </footer>
+  </body>
+</html>

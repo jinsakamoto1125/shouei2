fa49e77de8c5524b04503ecc765e2a62cc977b7c
sakamotojin1125 <saka1125jnjn@gmail.com>
2020-04-14 16:35:43 +0900

    fadein

--- index.php
+++ index.php
@@ -1,207 +1,253 @@
+
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
     <meta charset="utf-8">
     <title>昭栄電気産業</title>
-    <link rel="stylesheet" href="common/common.css">
-    <meta name="viewport" content="width=device-wide, initial-scale=1, shrink-to-fit=no">
-</head>
-<body style="background:#eaf4fc;">
-<header>
-
-    <div class="header-left">
-        <h1>SHO-<span>EI</span></h1>
-    </div>
-</header>
-<div class="top-wrapper">
-    <div class="top-wrapper-2">
-        <h1>皆さまの電気をお守りしています</h1>
-        <h2>私たちと一緒に働きませんか？</h2>
-    </div>
-</div>
-<div class="menu-wrapper" style="margin-top:10%;">
-    <div class="menu-wrapper-2 fadein" style="padding-left:10%;"><!--フェードインを使用して表示したいところでfadeinクラスを使用する-->
+     <link rel="stylesheet" href="common/common.css">
+     <meta name="viewport" content="width=device-wide, initial-scale=1, shrink-to-fit=no">
+  </head>
+  <body>
+      <header>
+        <div class="top-wrapper-1"  id="back">
+          <div class="header-left">
+            <h1>SHO-<span>EI</span></h1>
+          </div>
+          <div class="header-right" style="float:right; text-shadow : rgba(255,255,255,0.5) 0 5px 6px, rgba(255,255,255,0.2) 1px 3px 3px;
+          -webkit-background-clip : text;
+          font-size:17px; color:white;
+          -webkit-background-clip : text;  margin-right: 5%;  margin-top:2%;">
+          <p  style="float:left;  color:white;  padding-right:30px;">採用情報</p>
+          <p style="float:left;  color:white;   padding-right:30px">お問い合わせ</p>
+          <p style="float:left;  color:white;   padding-right:30px">会社概要</p>
+        </div>
+      </header>
+      <div class="top-wrapper"  id="beck">
+        <div class="top-wrapper-2">
+          <h1>街を明るく、</h1>
+          <h2 style="font-size:17px;  padding-top:20px;">Illuminate the world</h2>
+        </div>
+      </div>
+     </div>
+   <!--<div class="menu-wrapper" style="margin-top:10%;">
+      <div class="menu-wrapper-2" style="padding-left:10%;">
         <h1 style="font-size:20px;padding-top:10px; color:blue;">ABOUT</h1>
-        <h2 style="font-size:30px; padding-left:20px;">SHO-EIとは？</h2>
-    </div>
-    <div class="clear">
-    </div>
-    <div class="menu-wrapper-3" style="margin: 0 auto;  width:80%;">
+        <h2 style="font-size:40px; padding-left:20px;">SHO-EIとは？</h2>
+      </div>
+      <div class="clear">
+       </div>
+      <div class="menu-wrapper-3" style="margin: 0 auto;  width:80%;">
         <p style="font-size:50px; letter-spacing:4px; padding-top:6%;">お客様の安心を当たり前に</p>
         <p style="font-size:50px; letter-spacing:4px; padding-top:10px;">世の中を私たちが明るく</p>
         <p style="font-size:50px; letter-spacing:4px; padding-top:10px;">これからも皆様の電気をお守りしていきます</p>
-    </div>
-</div>
-<div class="lesson-wrapper" style="margin-top:10%">
-    <div class="lesson-wrapper-4" style="padding-left:10%;">
-        <h1 style="font-size:60px; float:left;">COMPANY</h1>
-        <h2 style="float:left; padding-top:3.5%;; font-size:20px; padding-left:3%; color:blue;">会社概要</h2>
-        <div class="clear">
-        </div>
-        <p style="font-size:60px; color:blue; font-weight:2000;">～</p>
-    </div>
+      </div>
+    </div>-->
+    <div id="about"  style="margin-top:10%;">
+    <div class="pcWrap fadein">
+      <div class="box"></div>
+       <div class="wrap">
+           <div class="left">
+               <div class="leftWrap">
+                   <h3>SHO-EIとは？</h3>
+                   <h4>Our Beliefs</h4>
+               </div>
+           </div>
+           <div class="center">
+               <div class="centerWrap">
+                   <h3>お客様の</h3>
+                   <h3>安心をあたりまえに.</h3>
+                   <p>It's a company that delivers electricity </p>
+                   <p>that is so bright that it is said to be ridiculous , stupid</p>
+               </div>
+           </div>
+           <div class="right"></div>
+           <br class="clear">
+       </div>
+   </div>
+   </div>
+ </div>
 
-    <div class="lesson-wrapper-2" style="margin:0  auto;  width:80%;">
-        <div class="lesson-wrapper-5" style="float:left;">
-            <img src="img/IMG_2657.JPG" alt="" style="width:500px; height:600px;">
-        </div>
-        <div class="lesson-wrapper-5" style="float:left;">
-            <div class="lesson-wrapper-3" style="float:left;">
-                <p style="font-size:30px;">会社名</p>
-                <p style="font-size:30px;">所在地</p>
-                <p style="font-size:30px;">売上高</p>
-                <p style="font-size:30px;">社員数</p>
-                <p style="font-size:30px;"></p>
-            </div>
-            <div class="lesson-wrapper-3" style=" float:left;  text-align:center;">
-                <p style="font-size:30px;">昭栄電気産業</p>
-                <p style="font-size:30px;"><a href="#">東京都葛飾区鎌倉3-58-2</a></p>
-                <p style="font-size:30px;">??億</p>
-                <p style="font-size:30px;">？？数</p>
-                <p style="font-size:30px;"></p>
-            </div>
-            <div class="clear">
-            </div>
-        </div>
-        <div class="clear">
-        </div>
-    </div>
-</div>
-<div class="good-wrapper">
-    <h1 class="fadein active">BUSINESS</h1>
-    <div class="good-wrapper-2">
-        <ul>
-            <li>
-                <div class="item">
-                    <a href="red">
-                        <img src="img/kuren.jpg" alt="" style="height:450px;">
-                        <p>保守</p>
-                    </a>
-                </div>
-                <div class="item">
-                    <a href="red">
-                        <img src="img/hozenn.jpg" alt="" style="height:225px;">
-                        <p>保全</p>
-                    </a>
-                </div>
-            </li>
-            <li class="item">
-                <a href="red">
-                    <img src="img/hosyu.jpg" alt="" class="good" style="height:225px;">
-                    <p>電気点検</p>
-                </a>
-            </li>
+    <div class="good-wrapper">
+        <h1 class="fadein">BUSINESS</h1>
+      <div class="good-wrapper-2 scroll-fade-row">
+           <ul>
+             <li>
+               <div class="item"  style="background:black;">
+                 <a href="red">
+                   <img src="img/kuren.jpg" alt=""  style="height:450px;">
+                   <p>保守</p>
+                 </a>
+               </div>
+               <div class="item" style="background:black;">
+                 <a href="red">
+                   <img src="img/hozenn.jpg" alt="" style="height:225px;">
+                   <p>保全</p>
+                 </a>
+               </div>
+             </li>
+             <li  class="item" style="background:black;">
+              <a href="red">
+               <img src="img/hosyu.jpg" alt=""  class="good" style="height:225px;">
+               <p>電気点検</p>
+             </a>
+           </li>
         </ul>
-        <div class="clear">
-
-        </div>
+         <div class="clear">
+         </div>
+      </div>
     </div>
-</div>
-<div class="message-wrapper">
-    <h1>RECRUIT</h1>
-    <div class="message-wrapper-2">
+    <div class="message-wrapper">
+        <h1  class="fadein">RECRUIT</h1>
+        <div class="message-wrapper-2">
         <ul>
-            <li class="item-2">
-                <img src="img/saiyou.jpg" alt="">
-                <a href="red">
-                    <p>中途採用</p>
-                </a>
-            </li>
-            <li class="item-2">
-                <img src="img/women.jpg" alt="">
-                <a href="red">
-                    <p>アルバイト・パート</p>
-                </a>
-            </li>
+          <li class="item-2">
+            <img src="img/saiyou.jpg" alt="" >
+            <a href="red">
+              <p>中途採用</p>
+            </a>
+          </li>
+          <li class="item-2">
+            <img src="img/women.jpg" alt="">
+            <a href="red">
+              <p>アルバイト・パート</p>
+            </a>
+          </li>
         </ul>
+      </div>
     </div>
-</div>
-<div class="clear">
+    <div class="clear">
 
-</div>
-<div class="contact">
-    <div class="contact-2">
-        <h2>ENTRY</h2>
-        <p>昭栄電気産業に関する募集要項とエントリーフォームはこちら。</p>
-        <div class="contact-3">応募する</div>
-        <div class="clear">
-        </div>
     </div>
-</div>
-<footer>
-    <div class="footer">
-        <div class="footer-2">
-            <div class="footer-3">
-                <p>理念</p>
-            </div>
-            <div class="footer-3">
-                <p><span>専門分野</span></p>
-                <p>造船・海洋</p>
-                <p>建設インフラ</p>
-                <p>エレクトロニクス</p>
-                <p>ヘルスケア・ライフサイエンス</p>
-                <p>食</p>
-                <p>生活</p>
-                <div class="clear">
-
-                </div>
-            </div>
-            <div class="footer-3">
-                <p><span>企業情報</span></p>
-                <p>トップメッセージ</p>
-                <p>戦略、中期経営計画</p>
-                <p>国内・海外ネットワーク</p>
-                <p>会社概要</p>
-                <p>沿革</p>
-                <p>大阪本社・西方ギャラリー</p>
-                <div class="clear">
-
-                </div>
-            </div>
-            <div class="footer-3">
-                <p><span>採用情報</span></p>
-                <p>新卒採用</p>
-                <div class="clear">
-
-                </div>
-            </div>
-            <div class="footer-3">
-                <p><span>お問い合わせ</span></p>
-                <p>ニュースリリース</p>
-                <p>プライバシーポリシー</p>
-                <div class="clear">
-
-                </div>
-            </div>
-            <div class="footer-3">
-                <p><span>language</span></p>
-                <div class="clear">
-
-                </div>
-            </div>
+    <div class="contact  fadein">
+       <div class="contact-2">
+         <h2>ENTRY</h2>
+         <p>昭栄電気産業に関する募集要項とエントリーフォームはこちら。</p>
+         <div class="contact-3  fadein">応募する</div>
+         <div class="clear">
+         </div>
         </div>
+       </div>
+     <footer>
+    <div class="footer fadein">
+      <div class="footer-2">
+      <div class="footer-3">
+        <p>理念</p>
+      </div>
+      <div class="footer-3">
+        <p><span>専門分野</span></p>
+        <p>造船・海洋</p>
+        <p>建設インフラ</p>
+        <p>エレクトロニクス</p>
+        <p>ヘルスケア・ライフサイエンス</p>
+        <p>食</p>
+        <p>生活</p>
         <div class="clear">
 
         </div>
-        <div class="footer-4">
-            <p>© 2019 HARADA CORPORATION All rights Reserved.</p>
+      </div>
+      <div class="footer-3">
+        <p><span>企業情報</span></p>
+        <p>トップメッセージ</p>
+        <p>戦略、中期経営計画</p>
+        <p>国内・海外ネットワーク</p>
+        <p>会社概要</p>
+        <p>沿革</p>
+        <p>大阪本社・西方ギャラリー</p>
+        <div class="clear">
+
         </div>
-    </div>
-</footer>
-<script src="common/js/jquery.js"></script>
-<script type="text/javascript">
-    $(function () {
-        $(window).scroll(function () {
-            $('.fadein').each(function () {
-                var position = $(this).offset().top;
-                var scroll = $(window).scrollTop(); //varがない
-                var windowHeight = $(window).height(); //varがない
-                if (scroll > position - windowHeight + 200) {
-                    $(this).addClass('active');
-                }
-            });
+      </div>
+      <div class="footer-3">
+        <p><span>採用情報</span></p>
+        <p>新卒採用</p>
+        <div class="clear">
+
+        </div>
+      </div>
+      <div class="footer-3">
+        <p><span>お問い合わせ</span></p>
+        <p>ニュースリリース</p>
+        <p>プライバシーポリシー</p>
+        <div class="clear">
+
+        </div>
+      </div>
+      <div class="footer-3">
+        <p><span>language</span></p>
+        <div class="clear">
+
+        </div>
+      </div>
+       </div>
+       <div class="clear">
+
+       </div>
+       <div class="footer-4">
+         <p>© 2019 HARADA CORPORATION All rights Reserved.</p>
+       </div>
+        </div>
+      </footer>
+      <script src="common/js/jquery.js"></script>
+      <script type="text/javascript">
+      $(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var position = $(this).offset().top;
+              scroll = $(window).scrollTop();
+                windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 80){
+              $(this).addClass('active');
+            }
+          });
+        });
+        $('#back').fadeIn(5000);
+        $('#beck').fadeIn(2000);
+        $(function(){
+
+    var effect_btm = 300; // 画面下からどの位置でフェードさせるか(px)
+    var effect_move = 50; // どのぐらい要素を動かすか(px)
+    var effect_time = 800; // エフェクトの時間(ms) 1秒なら1000
+
+    //親要素と子要素のcssを定義
+    $('.scroll-fade-row').css({
+        opacity: 0
+    });
+    $('.scroll-fade-row').children().each(function(){
+        $(this).css({
+            opacity: 0,
+            transform: 'translateY('+ effect_move +'px)',
+            transition: effect_time + 'ms'
         });
     });
-</script>
-</body>
-</html>
+
+    // スクロールまたはロードするたびに実行
+    $(window).on('scroll load', function(){
+        var scroll_top = $(this).scrollTop();
+        var scroll_btm = scroll_top + $(this).height();
+        var effect_pos = scroll_btm - effect_btm;
+
+        //エフェクトが発動したとき、子要素をずらしてフェードさせる
+        $('.scroll-fade-row').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( effect_pos > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+                $(this).children().each(function(i){
+                    $(this).delay(100 + i*200).queue(function(){
+                        $(this).css({
+                            opacity: 1,
+                            transform: 'translateY(0)'
+                        }).dequeue();
+                    });
+                });
+            }
+        });
+    });
+
+});
+        });
+          </script>
+        </body>
+        </html>

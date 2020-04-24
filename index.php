4991d936aa4f111a02cfb46b84ba48d3fe8a12ae
MizumuraYuhi <awphack@gmail.com>
2020-04-24 15:22:03 +0900

    jquery syntaxの原因となりそうな箇所を修正

--- index.php
+++ index.php
@@ -191,7 +191,7 @@
 <script type="text/javascript">
     //5秒かけて表示は遅すぎません
     $('.fadeleft').show(5000);
-      $('.fadeleft2'). addClass('active');
+      $('.fadeleft2').addClass('active');
     $(function () {
         $(window).scroll(function () {
             $('.fadein').each(function () {
